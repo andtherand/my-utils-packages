5531e0487172d0d85314ff5db45043dd86919015
Andreas Ruck <mychiara@gmail.com>
2015-02-10 19:13:31 +0100

    changed cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name		  	  "my utils packages"
+name		  	  "my_utils_packages"
 maintainer        "mychiara"
 maintainer_email  "mychiara+cookbooks@gmail.com"
 license           "Apache 2.0"
 description       "installs some basic packages like e.g. zip, vim and make"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
-recipe            "my-utils-packages", "installs some util packages"
+recipe            "my_utils_packages", "installs some util packages"
 
 %w{ debian ubuntu }.each do |os|
   supports os
